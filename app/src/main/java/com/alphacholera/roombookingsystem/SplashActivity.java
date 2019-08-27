500cd1b2effc398e1d915ceb4895386423c70405
Utkarsh Pankaj <alphacholera0201@gmail.com>
2019-08-28 02:47:52 +0530

    Added Firebase Authentication.

---
+++ app/src/main/java/com/alphacholera/roombookingsystem/SplashActivity.java
@@ -0,0 +1,77 @@
+package com.alphacholera.roombookingsystem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // For FireBase Authentication
+    private static final int RC_SIGN_IN = 1;
+    private FirebaseUser currentUser;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        FirebaseApp.initializeApp(this);
+
+        // Initializing the authentication components of FireBase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                if (currentUser == null) {
+                    startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setTheme(R.style.AppTheme)
+                    .setIsSmartLockEnabled(false)
+                    .setAvailableProviders(Arrays.asList(
+                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                            new AuthUI.IdpConfig.EmailBuilder().build()
+                    ))
+                    .build(), RC_SIGN_IN);
+                } else {
+                    // User already has an account
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }, 2000);
+                }
+            }
+        };
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+}
